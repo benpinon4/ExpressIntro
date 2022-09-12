1d664154aabc4aa02dda977b278508cdcc1fd5b5
benpinon4 <benjamin.pinon@CodeImmersives.com>
2022-09-12 08:35:01 -0400

    Part 2 Final Commit/Push

--- app.js
+++ app.js
@@ -1,44 +1,265 @@
 const express = require("express");
 const { route } = require("express/lib/application");
+
+const bodyParser = require('body-parser')
+
 const app = express();
 const port = 3000;
 const today = new Date();
-const favoriteMovieList = ["Starship Troopers", "Black Hawk Down"];
 
+const globalFirstName = " ";
+const globalLastName = " ";
 
-
-const movieString = favoriteMovieList.toString();
 
 app.get("/", (req, res) => {
   console.log("default route");
   res.send("Ben Piñon\n" + today);
 });
-app.get("/Movie-List", (req, res) => {
-  console.log("movie string route");
-  res.send(movieString);
-});
-app.get("/user-info", (req, res) => {
+
+app.get("/save-user-info", (req, res) => {
   console.log("user-info");
-  console.log(req.query);
-  res.send("User Info:  " + req.query.firstName + " " + req.query.lastName);
-});
-app.get("/add-movie", (req, res) => {
-  res.send("Added Movies: " + req.query.newMovie);
-  if (req.query.newMovie !== undefined) {//Condition checking to see if req.query.newMovie is undfined if so push into favoriteMovieList array.
-    favoriteMovieList.push(req.query.newMovie);
-    req.query.newMovie = undefined;//switch back to undefined so when return to add-movies it does not push undefined into favoriteMovieList array.
-  }
+  
+  globalFirstName = req.query.firstName
+  globalLastName = req.query.lastName
+  res.send("User Info:  " + globalFirstName + " " + globalLastName);
 });
 
-app.get("/show-movies", (req, res) => {
-  let finalListString = "";
-  for (let i = 0; i < favoriteMovieList.length-1; i++) {//converting movie list from array into string
-    finalListString += `${favoriteMovieList[i]}, `;
-  }
-  finalListString += favoriteMovieList[favoriteMovieList.length-1] //adding most current and last movie list item with no comma at the end.
-  res.send("Full Movie List: " + finalListString); 
+app.get("/show-user-info", (req, res) => {
+  console.log("default route");
+  res.send("User Info:  " + globalFirstName + " " + globalLastName);
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+
+const favoriteMovieList = [
+  {
+    title: "Star Wars",
+    starRating: 5,
+    isRecommended: true,
+    created: new Date(),
+    lastModified: new Date()
+  }, 
+  {
+    title:"Black Hawk Down",
+    starRating: 5,
+    isRecommended: true,
+    created: new Date(),
+    lastModified: new Date()
+  }];
+
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+
+
+// parse application/json
+app.use(bodyParser.json())
+
+
+//*****Data transfer via server*****//
+
+//Post or Add New
+app.post("/new-movie", (req, res) => {
+
+	console.log("POST to /new-movie")
+
+	
+	console.log("req.body ", req.body)
+
+
+	const newMovie = {
+		title: "",
+		starRating: 0,
+		isRecommended: false,
+		createdAt: new Date(),
+		lastModified: new Date()
+	}
+	
+	if (req.body.title === undefined) {
+		// Should trigger when req.body.title is undefined
+		console.log("title is not defined")
+		res.json({
+			success: false,
+			message: "title is a required field"
+		})
+		return;
+	} else {
+		console.log("title is defined")
+		newMovie.title = req.body.title
+	}
+
+	if (req.body.starRating === undefined) {
+		// Should trigger when req.body.starRating is undefined
+		console.log("starRating is not defined")
+		res.json({
+			success: false,
+			message: "starRating is a required field"
+		})
+		return;
+	} else {
+		console.log("starRating is defined")
+		newMovie.starRating = req.body.starRating
+	}
+
+	if (req.body.isRecommended === undefined) {
+		// Should trigger when req.body.isRecommended is undefined
+		console.log("isRecommended is not defined")
+		res.json({
+			success: false,
+			message: "isRecommended is a required field"
+		})
+		return;
+	} else {
+		console.log("isRecommended is defined")
+		newMovie.isRecommended = req.body.isRecommended
+	}
+
+	console.log("newMovie ", newMovie)
+	
+	favoriteMovieList.push(newMovie)
+
+	
+	res.json({
+		success: true
+	})
+})
+
+//Get or Read
+app.get("/all-movies", (req, res) => {
+
+	console.log("GET to /all-movies")
+
+	
+})
+
+app.get("/single-movie/:titleToFind", (req, res) => {
+	const titleToFind = req.params.titleToFind
+
+	const foundMovieIndex = favoriteMovieList.findIndex((movie)=>{
+
+		if (movie.title === titleToFind) {
+			console.log("Movie Titles Match!")
+			return true
+		} else {
+			console.log("Movie Titles Do Not Match")
+			return false
+		}
+	})
+
+	const foundMovie = favoriteMovieList[foundMovieIndex];
+
+	res.json(foundMovie)
+})
+
+// Update or Modify existing data
+
+app.put("/update-movie/:titleToUpdate", (req, res) => {
+
+	console.log("PUT to /update-movie")
+
+	console.log("req params ", req.params)
+
+	const titleToUpdate = req.params.titleToUpdate
+
+	
+	const originalMovieIndex = favoriteMovieList.findIndex((movie)=>{
+		console.log("movie ", movie)
+		console.log("titleToUpdate ", req.params.titleToUpdate)
+		console.log("condition ", movie.title === req.params.titleToUpdate)
+
+		if (movie.title === req.params.titleToUpdate) {
+			console.log("Movie Titles Match!")
+			return true
+		} else {
+			console.log("Movie Titles Do Not Match")
+			return false
+		}
+	})
+
+	console.log("originalMovieIndex ", originalMovieIndex)
+
+	const originalMovie = favoriteMovieList[originalMovieIndex];
+
+	console.log("originalMovie ", originalMovie)
+
+	const updatedMovie = {
+		title: originalMovie.title,
+		starRating: originalMovie.starRating,
+		isRecommended: originalMovie.isRecommended,
+		createdAt: originalMovie.createdAt,
+		lastModified: new Date()
+	}
+
+	console.log("updatedMovie Before Update ", updatedMovie)
+
+	if (req.body.title !== undefined) {
+		updatedMovie.title = req.body.title
+	}
+
+	if (req.body.starRating !== undefined) {
+		updatedMovie.starRating = req.body.starRating
+	}
+
+	if (req.body.isRecommended !== undefined) {
+		updatedMovie.isRecommended = req.body.isRecommended
+	}
+
+	console.log("updatedMovie After Update ", updatedMovie)
+
+	// Overwrite the value of favoriteMovieList at indexOfMovie with newTitle
+	// favoriteMovieList[indexOfMovie] = newTitle
+	favoriteMovieList[originalMovieIndex] = updatedMovie;
+
+	console.log("favoriteMovieList after ", favoriteMovieList)
+
+	res.json({
+		success: true
+	})
+})
+
+// Delete
+
+app.delete("/delete-movie/:titleToDelete", (req, res)=>{
+
+	console.log("DELETE to /delete-movie")
+
+	const indexOfMovie = favoriteMovieList.findIndex((movie)=>{
+
+		console.log(movie.title + " === " + req.params.titleToDelete)
+
+		if (movie.title === req.params.titleToDelete) {
+			console.log("Movie Titles Match!")
+			return true
+		} else {
+			console.log("Movie Titles Do Not Match")
+			return false
+		}
+	})
+	console.log(indexOfMovie)
+
+	if (indexOfMovie < 0) {
+		res.json({
+			hasBeenDeleted: false
+		})
+		return;
+	}
+
+	console.log("Before Delete ", favoriteMovieList)
+
+	favoriteMovieList.splice(indexOfMovie, 1)
+	console.log("After Delete ", favoriteMovieList)
+
+	res.json({
+		hasBeenDeleted: true
+	})
+})
+
+// // /*
+// // HTTP Rules (Hypertext Transfer Protocol)
+// // 1.
+
+
+
+
+// // */
+// // app.post("/new-movie", (req, res) =>)
